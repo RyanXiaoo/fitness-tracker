21eade6f8e50127518fbb296d19988a9cc60687b
RyanXiaoo <ryan.xiao12@hotmail.com>
2024-12-19 17:30:51 -0500

    hi

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,35 @@ import React from "react";
 import "./CSS/App.css";
 import "./CSS/navbar.css";
 import "./CSS/home.css";
+import "./CSS/card.css";
 import { useState, useEffect } from "react";
 
 const App = () => {
-    return (
-        <div className="background">
-            <Home />
-        </div>
-    );
+    const [page, setPage] = useState("home");
+
+    const renderPage = () => {
+        if (page === "home") {
+            return <Home setPage={setPage} />;
+        } else if (page === "legs") {
+            return <Legs setPage={setPage} />;
+        }
+    };
+
+    return <div className="background">{renderPage()}</div>;
 };
 
-function Nav() {
+function Nav({ setPage }) {
     return (
         <div className="navbar">
             <div className="leftnav">
-                <button className="icon">HOME</button>
+                <button className="icon" onClick={() => setPage("home")}>
+                    HOME
+                </button>
             </div>
             <div className="rightnav">
-                <button className="icon">LEGS</button>
+                <button onClick={() => setPage("legs")} className="icon">
+                    LEGS
+                </button>
                 <button className="icon">CHEST</button>
                 <button className="icon">BACK</button>
                 <button className="icon">OTHER</button>
@@ -28,10 +39,10 @@ function Nav() {
     );
 }
 
-function Home() {
+function Home({ setPage }) {
     return (
         <div>
-            <Nav />
+            <Nav setPage={setPage} />
             <div className="body1">
                 <div className="home-text">TRACK YOUR PROGRESS HERE</div>
                 <img
@@ -39,6 +50,25 @@ function Home() {
                     alt="Buff Guy"
                     className="animation"
                 />
+            </div>
+        </div>
+    );
+}
+
+function Legs({ setPage }) {
+    return (
+        <div>
+            <Nav setPage={setPage} />
+            <div className="container1">
+                <div className="leg1">
+                    <img src="/images/leg1.png" alt="" />
+                </div>
+                <div className="container2">
+                    <div className="card"></div>
+                </div>
+                <div className="leg2">
+                    <img src="/images/leg2.png" alt="" />
+                </div>
             </div>
         </div>
     );
